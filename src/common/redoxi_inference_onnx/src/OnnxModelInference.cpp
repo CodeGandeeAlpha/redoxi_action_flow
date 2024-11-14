1fde698454f57619288bf880f9d1eb67c4ac0f18
igamenovoer <igamenovoer@xx.com>
2024-11-14 16:47:08 +0000

    implementing OnnxModelInference

--- src/common/redoxi_inference_onnx/src/OnnxModelInference.cpp
+++ src/common/redoxi_inference_onnx/src/OnnxModelInference.cpp
@@ -14,45 +14,6 @@ KeyValueStore::Ptr OnnxModelInference::create_init_params()
     return std::make_shared<OnnxModelConfig>();
 }
 
-// ModelPortData::Ptr OnnxModelInference::create_model_port_data(
-//     const std::string& port_name,
-//     std::optional<int> batch_size)
-// {
-//     // find the port info, if not found, return nullptr
-//     auto it = m_port_infos.find(port_name);
-//     if (it == m_port_infos.end()) {
-//         return nullptr;
-//     }
-
-//     // create the port data
-//     auto output = std::make_shared<OnnxPortData>();
-
-//     // if batch size is provided, update the port info
-//     if (batch_size.has_value()) {
-//         auto port_info = it->second;
-//         auto shape = port_info->get_shape();
-//         if (!shape.empty() && shape[0] == -1) {
-//             auto new_port_info = std::make_shared<ModelPortInfo>(*port_info);
-//             new_port_info->get_shape()[0] = batch_size.value();
-//             output->m_port_info = new_port_info;
-//         } else {
-//             // if the port info is not dynamic, check if it matches the batch_size
-//             if (shape[0] == batch_size.value()) {
-//                 // ok, use the original port info
-//                 output->m_port_info = port_info;
-//             } else {
-//                 // the batch size does not match, regard it as failure
-//                 return nullptr;
-//             }
-//         }
-//     } else {
-//         // if batch size is not provided, use the original port info
-//         output->m_port_info = it->second;
-//     }
-
-//     return output;
-// }
-
 KeyValueStore::ConstPtr OnnxModelInference::get_model_metadata() const
 {
     return m_config;
