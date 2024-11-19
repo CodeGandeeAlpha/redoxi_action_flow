23656770ee0bc898acd067e29313a859f975a187
igamenovoer <igamenovoer@xx.com>
2024-11-19 11:31:14 +0000

    added detection model

--- src/common/redoxi_dnn_models/src/yolo8/Yolo8DetectionModel.cpp
+++ src/common/redoxi_dnn_models/src/yolo8/Yolo8DetectionModel.cpp
@@ -1,1 +1,9 @@
 #include <redoxi_dnn_models/yolo8/Yolo8DetectionModel.hpp>
+
+namespace redoxi_works::inference::yolo8
+{
+
+struct Yolo8DetectionModel::Impl {
+};
+
+} // namespace redoxi_works::inference::yolo8
