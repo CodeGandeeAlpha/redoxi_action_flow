4c1425c3ca2df917ccaf5776e145a67bc65da4c5
igamenovoer <igamenovoer@xx.com>
2025-04-07 09:09:08 +0000

    updated install-ros2.bash

--- scripts/yolo8-onnx-download.bash
+++ scripts/yolo8-onnx-download.bash
@@ -4,8 +4,21 @@ ScriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # download test model
 # ModelUrl="https://github.com/igamenovoer/test_models/raw/refs/heads/main/yolov8n-pose-640.onnx"
-ModelUrl="https://github.com/igamenovoer/test_models/raw/refs/heads/main/yolov8n-pose-dynbatch.onnx"
-ModelDstDir="${ScriptDir}/../tmp/models"
+# ModelUrl="https://github.com/igamenovoer/test_models/raw/refs/heads/main/yolov8n-pose-dynbatch.onnx"
+# ModelDstDir="${ScriptDir}/../tmp/models"
 
-mkdir -p ${ModelDstDir}
-wget --progress=bar:force ${ModelUrl} -O ${ModelDstDir}/yolov8n-pose-dynbatch.onnx
+# mkdir -p ${ModelDstDir}
+# wget --progress=bar:force ${ModelUrl} -O ${ModelDstDir}/yolov8n-pose-dynbatch.onnx
+
+model_urls=(
+    "https://huggingface.co/Xenova/yolov8-pose-onnx/resolve/main/yolov8s-pose.onnx?download=true"
+    "https://huggingface.co/Xenova/yolov8-pose-onnx/resolve/main/yolov8n-pose.onnx?download=true"
+)
+
+ModelDstDir="${ScriptDir}/../tmp/models"
+mkdir -p "${ModelDstDir}"
+
+for model_url in "${model_urls[@]}"; do
+    model_name=$(basename "${model_url%\?*}")
+    wget --progress=bar:force "${model_url}" -O "${ModelDstDir}/${model_name}"
+done
