23df024185953df72fb5a78f64da22378d9db6cf
igamenovoer <igamenovoer@xx.com>
2025-04-22 07:27:14 +0000

    updated

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -13,8 +13,18 @@ apt-get install -y libtbb-dev xtensor-dev
 # apt-get install -y nuget tensorrt-libs
 
 # required by the ros2_web_bridge
+PYTHON_VERSION=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+PIP_EXTRA_ARGS=""
+
+PYTHON_MAJOR=$(echo "$PYTHON_VERSION" | cut -d. -f1)
+PYTHON_MINOR=$(echo "$PYTHON_VERSION" | cut -d. -f2)
+
+if [ "$PYTHON_MAJOR" -gt 3 ] || { [ "$PYTHON_MAJOR" -eq 3 ] && [ "$PYTHON_MINOR" -ge 11 ]; }; then
+  PIP_EXTRA_ARGS="--break-system-packages"
+fi
+
 if [ ! -z "$http_proxy" ]; then
-    sudo pip3 install simplejpeg --proxy="$http_proxy"
+  sudo pip3 install simplejpeg --proxy="$http_proxy" $PIP_EXTRA_ARGS
 else
-    sudo pip3 install simplejpeg
+  sudo pip3 install simplejpeg $PIP_EXTRA_ARGS
 fi
